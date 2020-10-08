2881df16b5878c5b716a34acde30902ca5cd45cd
SamBrookeDev <kleychin@gmail.com>
2020-10-08 22:46:15 +0300

    add new. AdditionalTask Home Work Не совсем понял про обязательное приведение типов. Зачем?
    Добавлено 4 ДЗ Упражнение №2
    Добавлено 4 ДЗ Упражнение №3

--- src/HW_06/task3/Distance.java
+++ src/HW_06/task3/Distance.java
@@ -1,13 +1,43 @@
 package HW_06.task3;
 
+import java.util.Scanner;
+
 public class Distance {
     private double distance;
-    private double result;
 
-    public void setDistance(double distance) {
-        this.distance = distance;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("*** Конвертер расстояния ***");
+        System.out.println("============================");
+
+        System.out.print("Введите расстояние (в метрах) -> ");
+        int meters = input.nextInt();
+        System.out.println("Введите метод конвертации: 1 - мили, 2 - километры");
+        String method = input.next();
+
+        switch (method) {
+            case "1" : {
+                Converter.miles(meters);
+                break;
+            }
+            case "2" : {
+                Converter.kilometers(meters);
+                break;
+            }
+        }
+
     }
-    public static void Converter(double distance){
+
+    static class Converter {
+        static void miles(int m) {
+            double result = (double) m * 0.00062;
+            System.out.println("Расстояние " + m + " м равно " + result+ " миль");
+        }
+
+        static void kilometers(int m) {
+            double result = (double) m * 0.001;
+            System.out.println("Расстояние " + m + " м равно " + result+ " километров");
+        }
 
     }
 
