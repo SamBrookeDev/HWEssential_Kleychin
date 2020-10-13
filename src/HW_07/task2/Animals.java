2214ffd95feffba1761e97e621ac2063705bf6c1
SamBrookeDev <kleychin@gmail.com>
2020-10-13 21:10:15 +0300

    example commit

--- src/HW_07/task2/Animals.java
+++ src/HW_07/task2/Animals.java
@@ -4,8 +4,14 @@ enum Animals {
     ALLIGATOR(5), MONKEY(7), LION(3);
     int age;
 
-    Animals(int age){
+    Animals(int age) {
         this.age = age;
 
     }
+
+    @Override
+    public String toString() {
+        return "Animal -> " + super.toString() + " ,age -> " + age;
+    }
+
 }
